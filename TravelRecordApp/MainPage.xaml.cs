2e2b514f0754865262b6259d5a469eba600b4188
fajarcp <fajarfaju82@gmail.com>
2022-01-17 13:35:42 +0530

    1

--- TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/MainPage.xaml.cs
@@ -17,7 +17,16 @@ namespace TravelRecordApp
 
         private void loginButton_Clicked(object sender, EventArgs e)
         {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+            if(isEmailEmpty || isPasswordEmpty)
+            {
 
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
         }
     }
 }
