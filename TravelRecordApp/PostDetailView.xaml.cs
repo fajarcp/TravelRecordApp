878c50a8e84b458dcf6f30377d1d8c24173ba9a0
fajarcp <fajarfaju82@gmail.com>
2022-01-25 12:13:29 +0530

    sqlcomplete

---
+++ TravelRecordApp/PostDetailView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostDetailView : ContentPage
+    {
+        Post selectedPost;
+        public PostDetailView(Post postSelected)
+        {
+            InitializeComponent();
+            this.selectedPost = postSelected;
+            experienceEntry.Text = selectedPost.Experience;
+        }
+
+        private void updateButton_Clicked(object sender, EventArgs e)
+        {
+            selectedPost.Experience = experienceEntry.Text;
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(selectedPost);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Successful", "Experience updated successfully!", "ok");
+                }
+                else
+                {
+                    DisplayAlert("Failed", "Experience cannot be updated successfully!", "ok");
+                }
+            }
+
+        }
+
+        private void deleteButton_Clicked(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Delete(selectedPost);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Successful", "Experience deleted successfully!", "ok");
+                }
+                else
+                {
+                    DisplayAlert("Failed", "Experience cannot be deleted successfully!", "ok");
+                }
+            }
+        }
+    }
+}
--- TravelRecordApp/newTravelPage.xaml.cs
+++ TravelRecordApp/newTravelPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,29 @@ namespace TravelRecordApp
         public newTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+
+            };
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Successful", "Experience added successfully!", "ok");
+                }
+                else
+                {
+                    DisplayAlert("Failed", "Experience cannot be added successfully!", "ok");
+                }
+            }
         }
     }
 }
